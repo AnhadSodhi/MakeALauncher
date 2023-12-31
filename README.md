5f429afa214bcb3b0690ee3dd94d7606bf08ff4a
Anhad Sodhi <anhadsodhi2005@gmail.com>
2023-12-31 00:18:22 -0800

    Added setup.bat (creates the launchers) and updates the readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # MakeALauncher
 A simple program that lets the user create a file that launches multiple programs with just one click. Just put in the names of the programs you want, and let setup.bat do the rest. You don't even need the file paths!
+
+## Usage instructions
+* Download and launch setup.bat.
+* It will prompt you for the names of the programs you wish to add to the launcher, the drives to search, and the name of the output file. Enter them in as they appear.
+* The program will scan your computer for the locations of those programs and add them to the output file.
+* The computer will display a message once it is finished. Click anywhere to close that window.
+* That's it! You now have your very own custom launcher.
+
+## Tips
+* Abort the setup at any time using Ctrl+C. This will cause the output file to be incomplete. Delete it.
+* If you changed the locations of your programs after running setup, have no fear! Simply run setup.bat again and it will give you a new, updated output file.
+* It may take some time to find each program, especially if you have a lot of files.
+
+## Feel free to open an issue ticket (in the top menu) if you have any problems or questions!
+
+<sub>Most of this code was taken from my other project, BakkesLeague Launcher (https://github.com/frost-ee/BakkesLeague-Launcher). I wanted to make a version that wasn't hard-coded and instead let the user decide which programs to add to the output file.</sub>
---
+++ setup.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal enabledelayedexpansion
+
+echo Welcome to MakeALauncher. Follow the instruction to create your own custom launcher.
+set /p "search_programs=Which programs to put in the launcher? (Separate each one with a space. Capitalization does not matter. Make sure they each end with .exe) "
+set /p "drive_letters=Which drives to search? (Type only the drive letters, each separated with a space. Leave blank to search all of them) "
+set /p "output_batch_file=What to name the output file? (If a file already exists with that name, it will be replaced. Make sure it ends with .bat) "
+
+rem replace drive_letters with all drives if input is empty or only spaces
+if "%drive_letters%"=="" (
+    set "drive_letters=A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
+) else if not "%myVariable: =%"=="" (
+    set "drive_letters=A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
+)
+
+rem Delete the output batch file if it already exists
+if exist "%output_batch_file%" (
+	del "%output_batch_file%"
+	echo old %output_batch_file% deleted.
+)
+
+rem Iterate through all programs
+for %%p in (%search_programs%) do (
+	echo Searching for: %%p
+	
+	rem Iterate through all drives
+	for %%d in (%drive_letters%) do (
+		set "search_directory=%%d:\"
+		
+		rem Use dir /s /b to recursively search for the program path
+		for /f "tokens=*" %%f in ('dir /s /b "!search_directory!%%p" 2^>nul') do (
+			echo Found: %%f
+			echo start "" "%%f" >> "%output_batch_file%"
+		)
+	)
+)
+echo %output_batch_file% created. You may need to refresh the folder to make it appear.
+
+endlocal
+pause
