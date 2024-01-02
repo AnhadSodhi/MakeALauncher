c6c485bff6632061e00e9d1c19cb212c161b0763
Anhad Sodhi <anhadsodhi2005@gmail.com>
2024-01-01 21:21:20 -0800

    setup.bat now uses an input trimming subroutine instead of manually coding each trim

--- setup.bat
+++ setup.bat
@@ -4,75 +4,41 @@ setlocal enabledelayedexpansion
 echo Welcome to MakeALauncher. Follow the instructions to create your own custom launcher. Use enter to submit each input.
 
 set /p "search_programs=Which programs to put in the launcher? (Separate each one with a space. Capitalization does not matter. Make sure they each end with .exe) "
+call :trimSpaces search_programs
+
 set /p "drive_letters=Which drives to search? (Type only the drive letters, each separated with a space. Leave blank to search all of them) "
+call :trimSpaces drive_letters
+
 set /p "output_batch_file=What to name the output file? (If a file already exists with that name, it will be replaced. No spaces allowed. Make sure it ends with .bat) "
-
-rem Remove leading spaces for search_programs
-:trimStart
-if "!search_programs:~0,1!" == " " (
-    set "search_programs=!search_programs:~1!"
-    goto :trimStart
-)
-rem Remove trailing spaces for search_programs
-:trimEnd
-if "!search_programs:~-1!" == " " (
-    set "search_programs=!search_programs:~0,-1!"
-    goto :trimEnd
-)
-
-rem Remove leading spaces for drive_letters
-:trimStart2
-if "!drive_letters:~0,1!" == " " (
-	set "drive_letters=!drive_letters:~1!"
-	goto :trimStart2
-)
-rem Remove trailing spaces for drive_letters
-:trimEnd2
-if "!drive_letters:~-1!" == " " (
-	set "drive_letters=!drive_letters:~0,-1!"
-	goto :trimEnd2
-)
-
-rem Remove leading spaces for output_batch_file
-:trimStart3
-if "!output_batch_file:~0,1!" == " " (
-	set "output_batch_file=!output_batch_file:~1!"
-	goto :trimStart3
-)
-rem Remove trailing spaces for output_batch_file
-:trimEnd3
-if "!output_batch_file:~-1!" == " " (
-	set "output_batch_file=!output_batch_file:~0,-1!"
-	goto :trimEnd3
-)
+call :trimSpaces output_batch_file
 
 rem replace drive_letters with all drives if input is empty or only spaces
 if "%drive_letters%"=="" (
     set "drive_letters=A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
-) else if not "%myVariable: =%"=="" (
+) else if not "%drive_letters: =%"=="" (
     set "drive_letters=A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
 )
 
 rem Delete the output batch file if it already exists
 if exist "%output_batch_file%" (
-	del "%output_batch_file%"
-	echo old %output_batch_file% deleted.
+    del "%output_batch_file%"
+    echo old %output_batch_file% deleted.
 )
 
 rem Iterate through all programs
 for %%p in (%search_programs%) do (
-	echo Searching for: %%p
-	
-	rem Iterate through all drives
-	for %%d in (%drive_letters%) do (
-		set "search_directory=%%d:\"
-		
-		rem Use dir /s /b to recursively search for the program path
-		for /f "tokens=*" %%f in ('dir /s /b "!search_directory!%%p" 2^>nul') do (
-			echo Found: %%f
-			echo start "" "%%f" >> "%output_batch_file%"
-		)
-	)
+    echo Searching for: %%p
+    
+    rem Iterate through all drives
+    for %%d in (%drive_letters%) do (
+        set "search_directory=%%d:\"
+        
+        rem Use dir /s /b to recursively search for the program path
+        for /f "tokens=*" %%f in ('dir /s /b "!search_directory!%%p" 2^>nul') do (
+            echo Found: %%f
+            echo start "" "%%f" >> "%output_batch_file%"
+        )
+    )
 )
 
 rem Completion message
@@ -80,3 +46,20 @@ echo %output_batch_file% created. You may need to refresh the folder to make it 
 
 endlocal
 pause
+
+:trimSpaces
+rem Remove leading spaces
+:trimStart
+if "!%1:~0,1!" == " " (
+    set "%1=!%1:~1!"
+    goto :trimStart
+)
+
+rem Remove trailing spaces
+:trimEnd
+if "!%1:~-1!" == " " (
+    set "%1=!%1:~0,-1!"
+    goto :trimEnd
+)
+
+goto :eof
